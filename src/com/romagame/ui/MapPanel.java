1697a49a90db19c44eb295e9b4d039ad394a520b
Alex <89748403+Alexxiconify@users.noreply.github.com>
2025-07-06 13:04:00 -0600

    0.0.0.0.1.36

--- src/com/romagame/ui/MapPanel.java
+++ src/com/romagame/ui/MapPanel.java
@@ -80,8 +80,6 @@ public class MapPanel extends JPanel {
         loadProvinceOwnerColors();
         loadProvinceMask();
         setupMouseListeners();
-        setupWaterAnimation();
-        setupRepaintTimer();
         // Center on Buyeo's centroid
         SwingUtilities.invokeLater(() -> centerOnNation("Buyeo"));
     }
